43b7d24069ff50ba39b975e7cd65f5b757202e7b
1448761079@qq.com <1448761079@qq.com>
2020-06-18 13:09:03 +0800

    二次提交

---
+++ hzero-boot/hzero-boot-users/src/main/java/org/example/hzerobootusers/test/demo.java
@@ -0,0 +1,16 @@
+package org.example.hzerobootusers.test;
+
+import javax.xml.bind.annotation.XmlAnyAttribute;
+
+/**
+ * @ClassName: demo
+ * @Description: TODO
+ * @Author: Arvin
+ * @Date: 2020/6/18 11:22
+ * @Since version-1.0
+ **/
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("rtest");
+    }
+}
